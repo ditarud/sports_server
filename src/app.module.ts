7fc8b9f4bb0f50236aa631f2fbb6f865dc88b86e
Diego <diegotarud93@gmail.com>
2025-04-06 17:30:47 -0400

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DriversModule } from './modules/drivers/drivers.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [DriversModule],
+  imports: [DriversModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,17 +3,18 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './infraestructure/controllers/auth.controller';
 import { JwtStrategy } from './infraestructure/services/jwt.strategy';
+import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
 
 @Module({
   imports: [
-    PassportModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'super-secret',
       signOptions: { expiresIn: '1h' },
     }),
   ],
   controllers: [AuthController],
-  providers: [JwtStrategy],
+  providers: [JwtStrategy, JwtAuthGuard],
   exports: [JwtModule],
 })
 export class AuthModule {}
